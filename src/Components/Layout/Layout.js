358504c7c0771739bcc5cd2123c89c95a889653e
pelatge <pelat.g29@gmail.com>
2022-02-22 06:51:41 +0700

    add commponent/footer,header,main

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Layout(){
+	return(
+		<div>
+		<h1 className="text-red-600">Wellcome to simpleReact Blogs..</h1>
+		</div>
+
+	);
+}
+
+export default Layout;
