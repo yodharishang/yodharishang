0d178dfeed95ab3fa56c8c8a17274d73b0027ae4
pelatge <pelat.g29@gmail.com>
2022-02-22 15:41:30 +0700

    add NavbarHome.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer({children}){
 	return(
 		<>
-		<footer>
+		<footer className="bg-orange-700">
 		<h1>Ini Footer</h1>
 		{children}
 		</footer>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {NavBar} from '../NavBar';
+
 
 function Header({children}){
 	return(
 		<>
-		<header>
+		<header className="bg-blue-600">
 		<h1>Ini Header</h1>
+		<NavBar/>
 		{children}
 		</header>
 		</>
